da9992c7021cfe296ac4a4492e63d00f9c3bb4c7
Ernakh <fabriciotonettolondero@gmail.com>
2022-11-25 10:51:11 -0300

    first commit

---
+++ EntityPostgresCodeFirst/Contexto.cs
@@ -0,0 +1,45 @@
+﻿using EntityPostgresCodeFirst.DataModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityPostgresCodeFirst
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Email> Emails { get; set; }
+
+        public Contexto()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseNpgsql("User ID=postgres;Password=admin;Host=localhost;Port=5432;Database=EntityCodeFirst;Pooling=true;");
+
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["EntityPostgresql"];
+            string retorno = "";
+            
+            if (settings != null)
+                retorno = settings.ConnectionString;
+
+            optionsBuilder.UseNpgsql(retorno);
+
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Email>()
+                .HasOne(e => e.pessoa)
+                .WithMany(e => e.Emails)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}
